68c9214a3e8ca3ffd24e1db2adc2d07c2d242603
Valencia White <valenciawhite@rocketmail.com>
2022-02-10 15:48:35 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// const mainTag = document.getElementById('main');
+// const firstChild = mainTag.querySelector('main');
+// mainTag.removeChild(firstChild);
+
+//1. 
+document.querySelector('main').remove();
+
+//2. 
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+// document.body.append(newHeader);
+
+//3. 
+newHeader.textContent = 'Valencia is the champion';
+
+
